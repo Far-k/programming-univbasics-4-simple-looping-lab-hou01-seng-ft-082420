8aa9a2d5ea60abab20a1f7973358797642cfdf6b
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-21 18:01:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,11 +8,3 @@ def loop_message_five_times(message)
   puts message
 end
 
-counter = 0
-output_array = ["one should", "love thyself", "more", "than the material possession", "Hamster"]
-
-while output_array[counter] do
-  puts output_array[counter]
-  counter += 1
-end
-puts output_array

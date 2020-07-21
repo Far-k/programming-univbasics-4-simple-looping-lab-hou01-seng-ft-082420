ec24a18ba9756428e36aa153ae89e9983d7a2b75
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-21 16:37:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,10 +8,9 @@ def loop_message_five_times(message)
   puts message
 end
 
-message1 = ["Hello"]
-num = [3]
 
-def loop_message_n_times(message1, num)
-  3.times {puts "message1"}
+
+def loop_message_n_times(message = "Hello World", num = 5)
+  5.times {puts "message"}
 
 end

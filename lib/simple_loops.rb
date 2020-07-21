5985058ee6cfd6a4b878f4762aa000adab0e0386
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-21 15:55:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
-# Write your methods here
+message = ["Hello World"]
+
+def loop_message_five_times(message)
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+message1 = ["Hello"]
+
+def loop_message_n_times(message1)
+   counter = 0
+
+   while counter < 4 do
+     puts mesage[counter]
+     counter += 1
+   end
+end
